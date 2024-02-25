1b8710f90320d05cbaadfcda27b4628584c1dcb0
MinWook6457 <minuk6457@gmail.com>
2024-02-25 20:49:41 +0900

    [디비추가] : 댓글 테이블 추가

---
+++ src/main/java/com/insilicogen/CRUD_PRJ/bbs/service/Comment.java
@@ -0,0 +1,49 @@
+package com.insilicogen.CRUD_PRJ.bbs.service;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.insilicogen.CRUD_PRJ.user.service.User;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "CMNT")
+public class Comment extends BaseEntity{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cmntSn;
+	
+	@Column(name = "CMNT_CN")
+	@NotBlank
+	@Lob // Clob 설정
+	private String commentContent; // 댓글 내용
+	
+	@Column(name = "DEL_YN")
+	@NotNull
+	private Character isDeletedOption;
+	
+    @ManyToOne
+    @JoinColumn(name = "BOARD_SN")
+    private Board board;
+	
+    @ManyToOne
+    @JoinColumn(name = "USER_SN")
+    private User user;
+}
